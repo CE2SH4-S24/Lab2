51fa17f03bf8204270b80f5d47ac15dcfa84c3bc
SAtharMac <athars3@mcmaster.ca>
2024-05-16 13:59:07 -0400

    init commit

---
+++ Question3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Questions.h"
+
+
+// This is the very first question without hints in the comments.  Read the manual to develop your own algorithm.
+
+// Read Questions.h to understand the definition of Q3Struct and watch the Lab 2 briefing videos.
+
+void efficient(const int source[], struct Q3Struct effVector[], int size)
+{
+
+
+}
+
+void reconstruct(int source[], int m, const struct Q3Struct effVector[], int n)
+{
+
+
+}
